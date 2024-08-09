7f3744152b1060a84369faa7d3dd2b95a2dea3d5
Your Name <you@example.com>
2024-08-09 03:32:35 -0400

    Make car model

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,6 +12,16 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+from django.db import models
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    logo = models.ImageField(upload_to='car_makes/', blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +33,36 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('SPORTS', 'Sports'),
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('HATCHBACK', 'Hatchback'),
+        ('MINIVAN', 'Minivan'),
+        ('PICKUP', 'Pickup Truck'),
+        ('CROSSOVER', 'Crossover'),
+        ('ROADSTER', 'Roadster'),
+        ('LUXURY', 'Luxury'),
+        ('HYBRID', 'Hybrid'),
+        ('ELECTRIC', 'Electric'),
+        ('VAN', 'Van'),
+    ]
+    type = models.CharField(max_length=50, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2000)
+        ])
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    color = models.CharField(max_length=50, blank=True, null=True)
+    mileage = models.PositiveIntegerField(blank=True, null=True)
+    engine_size = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
